94d5903b5c53260efb7e8ed7117768d97ec2382c
liqiangqiang <liqiangqiang@youzan.com>
2019-05-15 17:50:56 +0800

    插入排序案例

---
+++ src/org/algorithm/sorting/InsertionSortingDemo.java
@@ -0,0 +1,54 @@
+package org.algorithm.sorting;
+
+import java.util.Arrays;
+
+/*
+ * Copyright [2015-2019] [泥瓦匠BYSocket]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * 插入排序案例
+ * <p>
+ * Created by bysocket on 19/5/15.
+ */
+public class InsertionSortingDemo {
+    
+    /**
+     * 插入排序
+     *
+     * @param arr
+     * @param <T>
+     */
+    public static <T extends Comparable> void insertionSort(T[] arr) {
+        int j;
+        
+        for (int p = 1; p < arr.length; p++) {
+            T tmp = arr[p];
+            for (j = p; j > 0 && tmp.compareTo(arr[j - 1]) < 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = tmp;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Integer[] intArr = new Integer[] {2, 3, 1, 4, 3};
+        
+        System.out.println(Arrays.toString(intArr));
+        insertionSort(intArr);
+        System.out.println(Arrays.toString(intArr));
+    }
+}
